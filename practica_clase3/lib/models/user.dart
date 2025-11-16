b5c43bd75696fd9f20ea817b5935e66eaacecf37
kate5926 <ksaicoc@ulasalle.edu.pe>
2025-11-15 20:10:17 -0500

    Agregar práctica clase 3 de Flutter

---
+++ practica_clase3/lib/models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String nombre;
+  String genero;
+  bool activo;
+  int edad;
+  String correo;
+
+  User({
+    required this.nombre,
+    required this.genero,
+    required this.activo,
+    required this.edad,
+    required this.correo,
+  });
+}
